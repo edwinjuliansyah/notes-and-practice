161de73eda4eaa40829fb6b55c937237bdbe315d
Emun <edwinjuliansyah273@gmail.com>
2026-05-18 21:40:01 +0700

    Refactor kasir.py for better formatting and logic
    menambahkan waktu, fitur kembalian, dan formatting

--- coding-practice/kasir.py
+++ coding-practice/kasir.py
@@ -1,4 +1,5 @@
-warkop = "=== SELAMAT DATANG DI WARKOP EMUN ==="
+from datetime import datetime
+warkop = f"{' SELAMAT DATANG DI WARKOP EMUN ':=^50}"
 total_belanja = 0
 keranjang = {}
 
@@ -7,22 +8,31 @@ def garis():
 
 def tampilkan_keranjang(massage):
   print(massage)
-for key, value in keranjang.items():
-    harga_format = f"Rp{value['harga']:,}".replace(",", ".")
-    subtotal_format = f"Rp{value['subtotal']:,}".replace(",", ".")
-    print(f"- {value['jumlah']} {value['nama']:<10} x {harga_format} = {subtotal_format:>8}")
-
-def buat_nota():
-  nota = ""
-  laporan = f" Nota Pembelian {nama_pembeli} "
-  nota += f"{laporan:=^{len(warkop)}}\n"
   for key, value in keranjang.items():
     harga_format = f"Rp{value['harga']:,}".replace(",", ".")
     subtotal_format = f"Rp{value['subtotal']:,}".replace(",", ".")
-    nota += f"- {value['jumlah']} {value['nama']:<10} x {harga_format} = {subtotal_format}\n"
+    print(f"- {value['jumlah']:<5} {value['nama']:<10} x {harga_format:<15} = {subtotal_format:>10}")
+  print(f"{'Total Belanja:':<30} {f'Rp{total_belanja:,}'.replace(',', '.'):>15}")
+
+def buat_nota(uang_bayar, uang_kembalian):
+  nota = ""
+  laporan = f" Nota Pembelian {nama_pembeli} "
+  nota += f"{laporan:=^{len(warkop)}}\n"
+  tanggal_waktu_sekarang = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+  nota += f"Tanggal Pembelian: {tanggal_waktu_sekarang}\n\n"
+
+  for key, value in keranjang.items():
+    harga_format = f"Rp{value['harga']:,}".replace(",", ".")
+    subtotal_format = f"Rp{value['subtotal']:,}".replace(",", ".")
+    nota += f"- {value['jumlah']:<5} {value['nama']:<10} x {harga_format} = {subtotal_format:>10}\n"
+  
   nota += "=" * len(warkop) + "\n"
   total_format = f"Rp{total_belanja:,}".replace(",", ".")
-  nota += f"Total Belanja: {total_format}"
+  nota += f"{'Total Belanja':<15}: {total_format:>30}\n"
+  uang_bayar = f"Rp{uang_bayar:,}".replace(",", ".")
+  uang_kembalian = f"Rp{uang_kembalian:,}".replace(",", ".")
+  nota += f"{'Uang Bayar':<15}: {uang_bayar:>30}\n"
+  nota += f"{'Uang Kembalian':<15}: {uang_kembalian:>30}"
   return nota
 
 print(warkop)
@@ -35,7 +45,9 @@ menu = {
 }
 
 for item in menu:
-    print(f"{item}. {menu[item]['nama']:<10} | Rp {menu[item]['harga']:>6}")
+    nama_menu = menu[item]['nama']
+    harga_menu = f"Rp{menu[item]['harga']:,}".replace(",", ".")
+    print(f"{item}. {nama_menu:<10} {'|':^25} {harga_menu:>10}")
 garis()
 
 nama_pembeli = input("Masukkan nama anda: ").strip()
@@ -84,11 +96,29 @@ while True:
   if pesanan_tambahan == "n":
     break
 
-print(buat_nota())
+while True:
+  try:
+    uang_bayar = int(input("Masukkan uang pembayaran: "))
+    if uang_bayar < total_belanja:
+      print("Uang tidak cukup, silakan masukkan jumlah yang sesuai")
+      continue
+    else:
+      break
+  except ValueError:
+    print("Input tidak valid, pastikan input berupa angka")
+    continue
+
+uang_kembalian = uang_bayar - total_belanja
+
+print(buat_nota(uang_bayar, uang_kembalian))
 garis()
-print(f"Terima kasih {nama_pembeli} telah memesan di Warkop Emun!\n")
+
+baris1 = f'Terima kasih {nama_pembeli}'
+baris2 = 'Sudah berkunjung di Warkop Emun!'
+print(f"{baris1:^{len(warkop)}}")
+print(f"{baris2:^{len(warkop)}}\n")
 
 with open("laporan_nota.txt", "a") as file:
     baris_akhir = ' Transaksi selesai '
-    end = f"{buat_nota()}\n{baris_akhir:=^{len(warkop)}}\n\n\n\n"
+    end = f"{buat_nota(uang_bayar, uang_kembalian)}\n{baris_akhir:=^{len(warkop)}}\n\n\n\n"
     file.write(end)
