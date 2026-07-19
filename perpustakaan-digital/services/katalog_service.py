dfcc1040c9794b0eee9be7efd0d73ff2f987c662
Emun <edwinjuliansyah273@gmail.com>
2026-07-19 16:07:14 +0700

    Menambahkan fungsi hapus dan test fungsi

--- perpustakaan-digital/services/katalog_service.py
+++ perpustakaan-digital/services/katalog_service.py
@@ -1,11 +1,18 @@
 from models.media import Media
 class KatalogService:
-    def __init__(self):
+    def __init__(self) -> None:
         self.katalog: dict[str, Media] = {}
 
-    def tambah_media(self, media: Media):
+    def tambah_media(self, media: Media) -> bool:
         self.katalog[media.id_media] = media
         return True
     
-    def cari_by_id(self, id_media):
+    def cari_by_id(self, id_media: str) -> Media | None:
         return self.katalog.get(id_media)
+    
+    def hapus_media(self, id_media: Media) -> bool:
+        if id_media in self.katalog:
+            del self.katalog[id_media]
+            return True
+        else:
+            return False
--- perpustakaan-digital/tests/test_katalog_service.py
+++ perpustakaan-digital/tests/test_katalog_service.py
@@ -9,3 +9,25 @@ def test_tambah_media_berhasil():
 
     assert hasil == True
     assert katalog.cari_by_id("B001") == buku
+
+def test_hapus_media_berhasil():
+    katalog = KatalogService()
+    buku = EbookLisensiBebas("Laskar Pelangi", "B001", {"Fiksi"}, "Andrea Hirata")
+
+    tambah = katalog.tambah_media(buku)
+
+    hapus = katalog.hapus_media(buku.id_media)
+
+    assert tambah == True
+    assert hapus == True
+    assert katalog.cari_by_id("B001") == None
+
+
+def test_hapus_media_gagal_jika_id_tidak_ada():
+    katalog = KatalogService()
+    buku = EbookLisensiBebas("Laskar Pelangi", "B001", {"Fiksi"}, "Andrea Hirata")
+    tambah = katalog.tambah_media(buku)
+
+    hasil = katalog.hapus_media("B009")
+    
+    assert hasil == False
